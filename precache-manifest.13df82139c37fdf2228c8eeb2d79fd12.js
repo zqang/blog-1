127dad17739bf303672270c33ffd817cb83cbb4a
zqang <alvinang1406@gmail.com>
2020-05-16 12:11:13 +0800

    Updates

---
+++ precache-manifest.13df82139c37fdf2228c8eeb2d79fd12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6a7a9bba46f32cf14064a8a846e963",
+    "url": "/blog-1/index.html"
+  },
+  {
+    "revision": "caefd2e318fb46950ed2",
+    "url": "/blog-1/static/css/main.cfd82947.chunk.css"
+  },
+  {
+    "revision": "48f048bb7c939ae8dd1f",
+    "url": "/blog-1/static/js/2.062561df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-1/static/js/2.062561df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caefd2e318fb46950ed2",
+    "url": "/blog-1/static/js/main.2c1fe71f.chunk.js"
+  },
+  {
+    "revision": "84e36f3dc8e6f5374625",
+    "url": "/blog-1/static/js/runtime-main.c85af3b5.js"
+  }
+]);
